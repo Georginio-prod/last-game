2612c5188ceb6eedda6d5610881700d82d5e3efc
Georginio <etonameklou6@gmail.com>
2024-05-19 01:50:32 +0000

    end paramètre

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,6 +10,11 @@ import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { FormField, Form, FormMessage } from '@/components/ui/form';
+import { useParams, useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { AlertModal } from '@/components/modals/alert.modal';
+import { ApiAlert } from '@/components/ui/api-alert';
 
 
 interface SettingsFormProps {
@@ -24,6 +29,10 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
+    const params = useParams();
+    const router = useRouter();
+
+
     const [open , setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -32,17 +41,47 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         defaultValues: initialData
     });
 
-    const onSubmit: SubmitHandler<SettingsFormValues> = (data) => {
-        // Handle form submission
-        console.log(data);
-        setLoading(true);
-        setTimeout(() => {
+    const onSubmit: SubmitHandler<SettingsFormValues> = async (data) => {
+        try {
+            console.log(data);
+            setLoading(true);
+            await axios.patch (`/api/stores/${params.storeId}`, data);
+            router.refresh();
+           toast.success("Boutique mise à jour")
+        }catch (error) {
+            toast.error ("quelque chose s'est mal passé.");
+        } finally {
             setLoading(false);
-        }, 2000);
+        }
+        // Handle form submission
+       
     };
+
+    const onDelete = async () => {
+        try{
+            setLoading(true)
+            await axios.delete(`/api/stores/${params.storeId}`)
+            router.refresh();
+            router.push("/")
+            toast.success("Boutique supprimer.")
+        }catch (error) {
+            toast.error("Assurez-vous d'abord d'avoir supprimé tous les produits et catégories.")
+        }finally  {
+            setLoading (false)
+            setOpen(false )
+        }
+    }
 
     return (
         <>
+        <AlertModal
+            isOpen= {open}
+            onClose={() => setOpen(false)}
+            onConfirm={onDelete}
+            loading= {loading}
+        />
+
+
             <div className="flex items-center justify-between">
                 <Heading
                     title="Paramètres"
@@ -83,9 +122,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
                 </Button>
             </form>
 
-           
-           
-            
+           <Separator/>
+           <ApiAlert 
+                title="NEXT_PUBLIC_API_URL"
+                description={`${origin}/api/${params.storeId}`}
+                variant='public' 
+            />
           
         </>
     );
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -14,6 +14,11 @@ export function MainNav ({
 
     const routes = [
         {
+            href : `/${params.storeId}`,
+            label: 'Aperçu',
+            active: pathname === `/${params.storeId}`
+        },
+        {
             href : `/${params.storeId}/settings`,
             label: 'Paramètres',
             active: pathname === `/${params.storeId}/settings`
